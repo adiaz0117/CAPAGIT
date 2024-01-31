6724333a788dc5fec6fbe2d5297ecd376d0e9bea
adiaz0117 <adiaz@isbelasoft.com>
2024-01-31 15:47:18 -0600

    ruta 1 configurada y editada

--- ruta1.js
+++ ruta1.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/ruta1', (req, res) => {
-    res.send(' desde ruta 1! te saludo');
+    res.send(' desde ruta 1! te saludo, modifcadaa');
 });
 
 module.exports = router;
