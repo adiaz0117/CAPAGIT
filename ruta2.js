e5669cc154421882fb99cdd1640261afb8ab6b8e
Jose Vargas <josev7810@puertoquetzal.gob.gt>
2024-01-31 15:47:07 -0600

    modificando ruta 2

--- ruta2.js
+++ ruta2.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/ruta2', (req, res) => {
-    res.send('adios Mundo desde ruta 2!');
+    res.send('Que onda Ale !');
 });
 
 module.exports = router;
